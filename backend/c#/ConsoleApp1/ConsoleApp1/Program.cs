fc12072a2bf3cd347a41c8d4d40ad2664850e055
vee/torio <134203950+veetorio@users.noreply.github.com>
2023-07-29 21:59:57 -0300

    meus arquivos

---
+++ backend/c#/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualBasic;
+using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+        for (int i = 0; i < 10; i++) 
+        {
+            Console.WriteLine(i);
+        }
+
+      
+
+    }
+
+}
