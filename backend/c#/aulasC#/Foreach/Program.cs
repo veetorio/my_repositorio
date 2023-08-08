77f8b2b6b6c93455b09c1bfdf0cc3e0d74eb7ada
vee/torio <134203950+veetorio@users.noreply.github.com>
2023-08-08 20:38:29 -0300

    j

--- backend/c#/aulasC#/Foreach/Program.cs
+++ backend/c#/aulasC#/Foreach/Program.cs
@@ -2,8 +2,16 @@
 
 class Program
 {
+    //ForEach é um tipo de estrutura derep 
     static void Main(string[] args)
     {
-        string[] nome = { "ettore", "izian", "leticia"}
+        string[] nomes = { "ettore", "izian", "leticia"};
+        Console.WriteLine("pesquise um nome:");
+        string sel_nome = Console.ReadLine();
+        foreach (string listnome in nomes)
+        {
+            console.WriteLine(listnome);
+        }   
     }
 }
+
