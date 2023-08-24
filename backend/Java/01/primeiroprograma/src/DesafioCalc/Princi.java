bb493a06172e79a55292c020087d1c5e1ecd7bb4
vee/torio <134203950+veetorio@users.noreply.github.com>
2023-08-24 05:51:37 -0300

    casa

---
+++ backend/Java/01/primeiroprograma/src/DesafioCalc/Princi.java
@@ -0,0 +1,21 @@
+package DesafioCalc;
+
+import java.sql.Struct;
+import java.util.Scanner;
+
+public class Princi
+{
+    public static void main(String[] args)
+    {
+        Scanner leia = new Scanner(System.in);
+        System.out.print("Digite seu operador (+,-,/,*,%) ");
+        String op = leia.next();
+
+        switch (op)
+        {
+            case "+":
+                
+        }
+
+    }
+}
